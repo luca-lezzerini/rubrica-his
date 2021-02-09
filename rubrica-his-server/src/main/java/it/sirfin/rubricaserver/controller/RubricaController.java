3238c4c604b5718e6821219a36ca38eaf9a27c9e
daniele-bit <daniele.fas95@gmail.com>
2021-02-09 12:01:56 +0100

    Controller aggiornato

--- rubrica-his-server/src/main/java/it/sirfin/rubricaserver/controller/RubricaController.java
+++ rubrica-his-server/src/main/java/it/sirfin/rubricaserver/controller/RubricaController.java
@@ -2,55 +2,42 @@ package it.sirfin.rubricaserver.controller;
 
 import it.sirfin.rubricaserver.dto.ReqContattoDto;
 import it.sirfin.rubricaserver.dto.ResRubricaDto;
-import java.util.ArrayList;
-import java.util.List;
+import it.sirfin.rubricaserver.service.RubricaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class RubricaController {
 
-    int cont = 0;
-    List<ReqContattoDto> preRubrica = new ArrayList<>();
-
-    @RequestMapping("/provarubrica")
-    public String provaRubrica() {
-        return "Il server funziona";
-    }
+    @Autowired
+    RubricaService rubricaService;
 
     @RequestMapping("/svuotarubrica")
-    public List<ReqContattoDto> svuotaRubrica() {
-        this.preRubrica = new ArrayList();
-        this.cont = 0;
-        return this.preRubrica;
+    @ResponseBody
+    public ResRubricaDto svuotaRubrica() {
+        return new ResRubricaDto(rubricaService.svuotaRubrica());
     }
 
     @RequestMapping("/inseriscicontatto")
-    public List<ReqContattoDto> inserisciContatto(@RequestBody ReqContattoDto contatto) {
-        contatto.setId(cont);
-        this.preRubrica.add(contatto);
-        cont ++;
-        return this.preRubrica;
-
+    @ResponseBody
+    public ResRubricaDto inserisciContatto(@RequestBody ReqContattoDto contatto) {
+        return new ResRubricaDto(rubricaService.inserisciContatto(contatto));
     }
 
     @RequestMapping("/recuperatuttiicontatti")
-    public List<ReqContattoDto> recuperContatti() {
-        return this.preRubrica;
+    @ResponseBody
+    public ResRubricaDto recuperContatti() {
+        return new ResRubricaDto(rubricaService.recuperaContatti());
     }
 
     @RequestMapping("/cancellacontatto")
-    public List<ReqContattoDto> cancellaContatto(@RequestBody ReqContattoDto dto) {
-        System.out.println("elemento da cancellare" + dto);
-        try {
-            this.preRubrica.removeIf(pr -> pr.getId() == dto.getId());
-            System.out.println("Elemento cancellato con successo");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return this.preRubrica;
+    @ResponseBody
+    public ResRubricaDto cancellaContatto(@RequestBody ReqContattoDto contatto) {
+        return new ResRubricaDto(rubricaService.cancellaContatto(contatto));
     }
 }
